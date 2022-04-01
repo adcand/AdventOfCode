edde8cd8800b4886c953c20452e872e16f6a5a32
Adriano <66135261+adcand@users.noreply.github.com>
2022-03-31 16:12:36 -1100

    added the second part of the challenge

--- 2020/day1.js
+++ 2020/day1.js
@@ -19,18 +19,42 @@ const nums = [
 
 let n = 0;
 
-!function sum() {
+!(function sum1() {
   for (num of nums) {
     let r = nums[n] + num;
 
     if (r === 2020) {
-      console.log(nums[n] * num); 
-      break;
+      console.log(nums[n] * num);
+      return;
     }
 
     if (num === nums[nums.length - 1] && n < nums.length) {
       n++;
-      sum();
+      sum1();
     }
   }
-}();
+})();
+
+// Part two
+
+let n2 = 0;
+
+!(function sum2() {
+  for (num of nums) {
+    let r = nums[n2] + num;
+
+    if (r < 2020) {
+      for (nu of nums) {
+        if (r + nu === 2020) {
+          console.log(nums[n2] * num * nu);
+          return;
+        }
+      }
+    }
+
+    if (num === nums[nums.length - 1] && n2 < nums.length) {
+      n2++;
+      sum2();
+    }
+  }
+})();
