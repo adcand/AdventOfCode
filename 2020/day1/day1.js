0b81de6861ece9e27e35f3175447710ab61fef44
Adriano <66135261+adcand@users.noreply.github.com>
2022-04-08 23:55:38 -1100

    deleted repeated code

--- 2020/day1/day1.js
+++ 2020/day1/day1.js
@@ -11,33 +11,20 @@ let n = 0;
       return;
     }
 
+    // Part two
+
+    // if (r < 2020) {
+    //   for (nu of nums) {
+    //     if (r + nu === 2020) {
+    //       console.log(nums[n] * num * nu);
+    //       return;
+    //     }
+    //   }
+    // }
+
     if (num === nums[nums.length - 1] && n < nums.length) {
       n++;
       sum1();
-    }
-  }
-})();
-
-// Part two
-
-let n2 = 0;
-
-!(function sum2() {
-  for (num of nums) {
-    let r = nums[n2] + num;
-
-    if (r < 2020) {
-      for (nu of nums) {
-        if (r + nu === 2020) {
-          console.log(nums[n2] * num * nu);
-          return;
-        }
-      }
-    }
-
-    if (num === nums[nums.length - 1] && n2 < nums.length) {
-      n2++;
-      sum2();
     }
   }
 })();
